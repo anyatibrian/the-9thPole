da5c72a3bc7246c5ea036547cfe8df92810b6bed
anyatibrian <briananyati@anyatibrians-MacBook-Pro.local>
2019-08-29 19:01:39 +0300

    ch(setup-project):setup the project using. - setup typescript - setup sequelize - setup nodemon - configure express for the project   [maintians #]

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import bodyParser from 'body-parser'
+import express, { Express, Request, Response } from 'express'
+import morgan from 'morgan'
+
+// setting up the application
+const app: Express = express()
+const port = 8080
+app.use(morgan('dev'))
+// setting up the application with body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+// the initial app setup
+app.get('/', (req: Request, res: Response) => {
+  return res.status(200).json({
+    message: 'your welcome to the app',
+  })
+})
+app.listen(port, () => console.log(`listing at port:http://localhost:${port}`))
